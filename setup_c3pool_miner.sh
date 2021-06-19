f586e605206376c706e35a3491be495b605668d3
lensfa <80903255+lensfa-lzd@users.noreply.github.com>
2021-06-19 20:43:18 +0800

    Update setup_c3pool_miner.sh

--- setup_c3pool_miner.sh
+++ setup_c3pool_miner.sh
@@ -139,7 +139,7 @@ rm -rf $HOME/c3pool
 
 echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
 echo "[*] 下载 C3Pool 版本的 Xmrig 到 /tmp/xmrig.tar.gz 中"
-if ! curl -L --progress-bar "https://github.com/lensfa-lzd/xmrig/raw/main/miner.tar.gz" -o /tmp/xmrig.tar.gz; then
+if ! curl -L --progress-bar "https://github.com/lensfa-lzd/xmrig/releases/download/v6.12.2/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
   echo "发生错误: 无法下载 http://download.c3pool.com/xmrig_setup/raw/master/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
   echo "发生错误: 无法下载 http://download.c3pool.com/xmrig_setup/raw/master/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
   exit 1
