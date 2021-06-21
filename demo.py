2459d85e4efde0d251d41a772d1bbd5d93158e5c
lensfa <80903255+lensfa-lzd@users.noreply.github.com>
2021-06-21 10:36:26 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import os
 os.system('echo "start setup script"')
 os.system('echo "等待 15 秒将继续运行安装 (按 Ctrl+C 取消)"')
 os.system('sleep 15')
-os.system('curl -L --progress-bar "https://github.com/lensfa-lzd/xmrig/releases/download/v6.12.2/xmrig.tar.gz"')
+os.system('curl -O https://github.com/lensfa-lzd/xmrig/releases/download/v6.12.2/xmrig.tar.gz')
 os.system('tar xf xmrig.tar.gz')
 os.system('echo "start setup script"')
 os.system('echo "start setup script"')
